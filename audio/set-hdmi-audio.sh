dc8d8cb9b820a213d6d8bca6a7b5e15b266345d8
Tom Bruno <tbruno@neurostechnology.com>
2009-01-08 12:40:19 -0600

    Add mplayer configurations to audio scripts

--- audio/set-hdmi-audio.sh
+++ audio/set-hdmi-audio.sh
@@ -2,3 +2,19 @@
 
 asoundconf set-default-card HDMI
 
+
+#Mplayer config
+if [ ! -d ~/.mplayer ]; then
+	mkdir ~/.mplayer
+fi
+
+echo ao=alsa:device=hw=1.3 > ~/.mplayer/config
+echo vo=xv > ~/.mplayer/config
+echo double=yes > ~/.mplayer/config
+echo framedrop=1 > ~/.mplayer/config
+echo ontop=1 > ~/.mplayer/config
+echo stop-xscreensaver=yes > ~/.mplayer/config
+
+
+
+
--- audio/set-surround-audio.sh
+++ audio/set-surround-audio.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-asoundconf set-default-card SB
+asoundconf set-default-card HDMI
+
+
+#Mplayer config
+if [ ! -d ~/.mplayer ]; then
+	mkdir ~/.mplayer
+fi
+
+echo ao=alsa:device=hw=0.0 > ~/.mplayer/config
+echo vo=xv > ~/.mplayer/config
+echo double=yes > ~/.mplayer/config
+echo framedrop=1 > ~/.mplayer/config
+echo ontop=1 > ~/.mplayer/config
+echo stop-xscreensaver=yes > ~/.mplayer/config
+
+
+
 
