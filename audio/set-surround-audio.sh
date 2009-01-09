46be52056e98e33bf149badbec34596565dad04e
Tom Bruno <tbruno@neurostechnology.com>
2009-01-09 12:56:41 -0600

    Change mistake in mplayer configs that made each line the first line of a file instead of appended

--- audio/set-surround-audio.sh
+++ audio/set-surround-audio.sh
@@ -9,11 +9,11 @@ if [ ! -d ~/.mplayer ]; then
 fi
 
 echo ao=alsa:device=hw=0.0 > ~/.mplayer/config
-echo vo=xv > ~/.mplayer/config
-echo double=yes > ~/.mplayer/config
-echo framedrop=1 > ~/.mplayer/config
-echo ontop=1 > ~/.mplayer/config
-echo stop-xscreensaver=yes > ~/.mplayer/config
+echo vo=xv >> ~/.mplayer/config
+echo double=yes >> ~/.mplayer/config
+echo framedrop=1 >> ~/.mplayer/config
+echo ontop=1 >> ~/.mplayer/config
+echo stop-xscreensaver=yes >> ~/.mplayer/config
 
 
 
